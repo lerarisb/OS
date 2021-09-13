e8d1c119224d191608029f89f25993a8e267fcd2
cdshelly00 <shelgrenc@xavier.edu>
2021-09-13 16:18:01 -0700

    reupload of comments

--- pandos/phase1/pcb.c
+++ pandos/phase1/pcb.c
@@ -2,36 +2,57 @@
 #include "../h/types.h"
 #include "../h/pcb.h"
 
-HIDDEN pcb_PTR, pcbFree_h;
+HIDDEN pcb_PTR pcbFree_h;
 
 void freePcb(pcb_t *p){
+    //insert element pointed to by p onto pcbFree list
+    //pcb is no longer in use, being removed from queue
+
+    //check to make sure pcb free list isn't null (that there are pcbs)
+    //if it is null, p becomes the first element in list
     if(pcbFree_h == NULL) {
-        pcbFree_h = p;
+        pcbFree_h = *p;
         return;
     }
+    //add to the head of the linked list of pcbs maintained in pcb free
     else{
-        p->p_next = pcbFree_h;
-        pcbFree_h = p;
+        //make p the first element
+        //p points to the old first element
+        *p->p_next = *pcbFree_h;
+        *pcbFree_h = *p;
     }
 }
 
 pcb_t *allocPcb(){
-    if(pcbFree_h == NULL){
+    //allocated a pcb from the free list to the queue of active pcbs
+
+    //check to make sure there is a pcb to remove from free list
+    if(*pcbFree_h == NULL){
         return NULL;
     }
-    pcb_t temp;
-    temp = pcbFree_h;
-    pcbFree_h = pcbFree_h->p_next;
+
+    //remove the first element from pcb free and add to queue
+    //removes any previous values from pcb
+
+    pcb_t *temp;
+    *temp = *pcbFree_h;
+
+    //setting the pointer to the free list to the next pcb
+    //essentially removing the current head on free list
+    *pcbFree_h = *pcbFree_h->p_next;
+
+    //all previous values of pcb are set to null
     temp->p_child = NULL;
     temp->p_sib = NULL;
-    temp->p_prevsib = NULL;
     temp->p_prnt = NULL;
     temp->p_next = NULL;
     temp->p_prev = NULL;
-    return temp;
+    return *temp;
 }
 
 initPcbs(){
+    //initalize the pcbFree list to conatain all the elements of the static array of MAXPROC pcbs
+    //only called once during data initalization
     static pcb_t pcbInit[MAXPROC];
     pcbFree_h = NULL;
     int i = 0;
@@ -47,23 +68,20 @@ pcb_t *mkEmptyProcQ(){
 
 int emptyProcQ(pcb_t *tp){
     //tp points to tail of queue, if queue is empty then return true
-    if (*tp == null){
-        return TRUE
-    }
-    else{
-        return FALSE
-    }
+    return (tp == null);
+
 
 }
-
+//inserting a pcb into the process queue
+//given the tail pointer of the process queue and the pcb we want to insert
 insertProcQ(pcb_t **tp, pcb_t *p){
-    //*tp points to tail, *p is the one being inserted
+    //*tp points to tail , *p is the one being inserted
     //must store old tail
     pcb_t *oldTail = *tp;
     //store head for convenience, head is pcb after tail
     pcb_t *head = *tp->p_next;
     //tp points to a tail, want to to point to new tail
-    **tp = *p;
+    (*tp) = *p;
     //new tail must point to same head
     *p->p_next = *head;
     //new tail must point previously to oldtail
@@ -71,8 +89,9 @@ insertProcQ(pcb_t **tp, pcb_t *p){
 
 
 }
-
+//removes the process from the head
 pcb_t *removeProcQ(pcb_t **tp){
+    //if there are no processes in the queue, there are none to remove
     if (emptyProcQ(*tp)){
         return NULL;
     }
@@ -91,82 +110,108 @@ pcb_t *removeProcQ(pcb_t **tp){
         *tp->p_next->p_prev = *tp;
     }
     
-
+    
 }
 
+//removes a specified pcb (p) from the queue pointed to by a tail pointer (tp)
 pcb_t *outProcQ(pcb_t **tp, pcb_t *p){
+    //if the queue has no processes, there are none to be removed
     if (emptyProcQ(*tp)){
         return Null;
 }
     else{
-        //store a current node
+        //store a current node (starting with the head) before we traverse through the queue
         pcb_t *currentPcb = *tp->p_next;
 
-        if (*tp = *p){
+//if the tail pointer is the pcb you want to remove
+        if (*tp == *p){
             //remove because desired case
             //store old tail
             pcb_t *oldTail = *tp;
 
             //old tail prev becomes new tail
-            *tp =  *oldTail->p_prev;
+           (*tp)=  *oldTail->p_prev;
 
             //new tail next points to what old tail did
+            
             *tp->p_next = *oldTail->p_next;
 
             //head previous points to new tail
             *tp->p_next->p_prev = *tp;
         }
 
-        //the test is, if the current pcb is node the last pcb
-        //basically, checking to make sure you haven't gone through the whole queue
+        //we continue to traverse until we hit the tail pointer
+        //if you have not hit the tail pointer but the current pcb is not what you want to remove, keep traversing
         while (*currentPCb != *tp){
             //for each current pcb, check to see if it is the desired pcb
             //if it is, remove it
             if (*currentPcb != *p){
-                //remove it
+                //store the one before the current pcb
+                pcb_t *prevToRemovalPCB = *currentPcb->p_prev;
+                //store the pcb after the current pcb
+                pcb_t *afterToRemovalPCB = *currentPcb->p_next;
+
+                //set the next of previous pcb to the next pcb
+                *prevToRemovalPCB->p_next = *afterToRemovalPCB;
+
+                //set the previous of the next pcb to be the previous pcb
+                *afterToRemovalPCB->p_prev = *prevToRemovalPCB;
+
             }
-            //if not, go to next pcb
+            //if current pcb not what searching for, go to next pcb
+            
             else{
                 *currentPcb = currentPcb -> p_next;
             }
             }
+            return NULL:
         }
     }
-}
 
+//return a pointer to first pcb from process queue whose tail is pointed to by a given tail pointer
 pcb_t *headProcQ(pcb_t *tp){
+    //checks to see if there are processes in the process queue
     if (emptyProcQ(*tp))
         return (null);
+    //returns head of process queue
     pcb_t *head = *tp->p_next;
     return *head; 
 
 }
-
-int emptyChile(pcb_t *p){
-    if (*p.p_child == NULL)
-        return TRUE
-    return FALSE
+//determines if a pcb has any children
+int emptyChild(pcb_t *p){
+    return (*p->p_child == NULL);
 }
 
+//adds a child to a pcb that is in process queue
 insertChild(pcb_t *prnt, pcb_t *p){
     //check to see if prnt already has a child
-    if (emptyChile(*prnt))
-        *prnt.p->p_child = *p;
-    *p.p->p_prnt = *prnt;
-    //prnt already has a child
-    else{
-        pcb_t *currentChild = *prnt->p_child;
-        //set child to its parent
+    if (emptyChild(*prnt))
+
+    //if parent does not have a child, add the pcb as one
         *prnt->p_child = *p;
-        //set child to its sibling (old child)
-        *prnt->p.child->p_sib = *currentChild;
+        *p->p_parent = *prnt;
+        *p_sib = NULL;
+
+        
+    else{
+        //if parent has a child, add pcb as child and move existing children
+        pcb_t *currentChild =  *prnt->p_child;
+
+    //set new pcb as child
+        *prnt->p_child = *p;
+        //set new child to have parent
+        *p->p_parent = *prnt;
+        //recently added pcb must be a sbiling to the existing child
+        *prnt->p_child->p_sib = *currentChild;
 
     }
 }
-
+//remove the head child
 pcb_t *removeChild(pcb_t *p){
     //test to see if p has child
-    if (emptyChile(*p)){
+    //if p has no child, return null
+    if (emptyChild(*p))
         return NULL;
     }
     //test to see if p has more than one child
@@ -177,6 +222,7 @@ pcb_t *removeChild(pcb_t *p){
         *currentChild->p_sib->p_parent = *p;
         //set the child of the parent to be the old sibling
         *p->p_child = *currentChild->p_sib;
+
         return *currentChild;
     }
     //otherwise p has exactly one child
@@ -185,16 +231,14 @@ pcb_t *removeChild(pcb_t *p){
     *p->p_child->p_parent = NULL; 
     //take parent and set child to null
     *p->p_child = NULL;
-    return *currentChild;
-}
+    return*currentChild;
 
+//removes a specific child
 pcb_t *outChild(pcb_t *p){
     //check to see if p has parent
     if (*p->p_parent == NULL){
         return null;
     }
-    //p has a parent
-    pcb_t *parent = *p->p_parent;
-    //remove the child of p
-    removeChild(*parent);
+    //remove the child
+    removeChild(*p);
 }
