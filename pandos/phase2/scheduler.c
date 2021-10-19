08af08806bd57f73824c0e518803d3c287773ca9
cdshelly00 <shelgrenc@xavier.edu>
2021-10-18 17:50:47 -0700

    scheduler

--- pandos/phase2/scheduler.c
+++ pandos/phase2/scheduler.c
@@ -1,1 +1,43 @@
 /* scheduler */
+
+
+#include "../h/const.h"
+#include "../h/types.h"
+#include "../h/asl.h"
+#include "../h/pcb.h"
+
+/* Variables used from initial.c */
+extern int processCount;
+extern int softBlockCount;
+extern pcb_t *currentProc;
+extern pcb_t *readyQueue;
+
+/* Uses the round robin algorithm for each process that will be executed */
+void scheduler(){
+	/* temp variable for the pcb that will be removed */
+	pcb_t *temp;
+
+	/* removes pcb from head of ready queue and stores the pointer in temp */
+	temp = removeProcQ(&readyQueue);
+
+	/* If the process count is 0 then the job is completed so you can halt */
+	if(processCount == 0){
+		HALT();
+	}
+
+	else if (processCount > 0){
+		if(softBlockCount > 0){
+			/* need to set timer still */
+
+			/* enable all interrupts I believe */
+			/* not sure if this is used to enable the interrupts or PLT */
+			unsigned int sendStatus = ALLON | IECON | IMON | TEBITON;
+
+			WAIT();
+		}
+		/* there is a deadlock */
+		if(softBlockCount == 0){
+			PANIC();
+		}
+	}
+}
